fa33c03af8496fc6b03bce1b01a7fea03523a504
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-07-17 10:31:15 -0500

    Write IDAT

--- src/minpng.py
+++ src/minpng.py
@@ -15,9 +15,19 @@ PNG_COLOR_TYPE_GRAYSCALE = 0
 
 def write_minimum_png(out):
     write_signature(out)
-    write_ihdr(out, 1, 1, 1, PNG_COLOR_TYPE_GRAYSCALE)
-    # write_foo(out)
+    width = 1
+    height = 1
+    bit_depth = 1
+    color_type = PNG_COLOR_TYPE_GRAYSCALE
+    write_ihdr(out, width, height, bit_depth, color_type)
+    write_one_black_pixel(out, bit_depth, color_type)
     write_iend(out)
+
+
+def write_one_black_pixel(out, bit_depth, color_type):
+    if color_type == PNG_COLOR_TYPE_GRAYSCALE and bit_depth == 1:
+        scanlines = bytes([0])
+    write_idat(out, scanlines, filter_none)
 
 
 PNG_SIGNATURE = [137, 80, 78, 71, 13, 10, 26, 10]
@@ -30,16 +40,16 @@ def write_signature(out):
 
 PNG_CHUNK_TYPE_IHDR = b'IHDR'
 def write_ihdr(out, width, height, bit_depth, color_type):
-	data = b''.join([
-		four_byte(width),
-		four_byte(height),
-		one_byte(bit_depth),
-		one_byte(color_type),
-		one_byte(0), # deflate/inflate compression with a sliding window of at most 32768 bytes
-		one_byte(0), # adaptive filtering with five basic filter types
-		one_byte(0), # no interlace
-	])
-	write_chunk(out, PNG_CHUNK_TYPE_IHDR, data)
+    data = b''.join([
+        four_byte(width),
+        four_byte(height),
+        one_byte(bit_depth),
+        one_byte(color_type),
+        one_byte(0), # deflate/inflate compression with a sliding window of at most 32768 bytes
+        one_byte(0), # adaptive filtering with five basic filter types
+        one_byte(0), # no interlace
+    ])
+    write_chunk(out, PNG_CHUNK_TYPE_IHDR, data)
 
 
 PNG_CHUNK_TYPE_IEND = b'IEND'
@@ -47,16 +57,27 @@ def write_iend(out):
     write_chunk(out, PNG_CHUNK_TYPE_IEND, b'')
 
 
+PNG_CHUNK_TYPE_IDAT = b'IDAT'
+def write_idat(out, scanlines, filter):
+    data1 = filter(scanlines)
+    data2 = zlib.compress(data1)
+    write_chunk(out, PNG_CHUNK_TYPE_IDAT, data2)
+
+
+def filter_none(scanlines):
+    return b'\0' + scanlines
+
+
 def four_byte(i):
-	return to_bytes(i, 4)
+    return to_bytes(i, 4)
 
 
 def one_byte(i):
-	return to_bytes(i, 1)
+    return to_bytes(i, 1)
 
 
 def to_bytes(i, length):
-	return int(i).to_bytes(length, 'big', signed=False)
+    return int(i).to_bytes(length, 'big', signed=False)
 
 
 def write_chunk(out, chunk_type, chunk_data):
@@ -69,17 +90,17 @@ def write_chunk(out, chunk_type, chunk_data):
 
 
 def calculate_crc(chunk_type, chunk_data):
-	c1 = zlib.crc32(chunk_type)
-	c2 = zlib.crc32(chunk_data, c1)
-	return c2
+    c1 = zlib.crc32(chunk_type)
+    c2 = zlib.crc32(chunk_data, c1)
+    return c2
 
 
 def write_four_byte(out, word):
-	out.write(word)
+    out.write(word)
 
 
 def write_chunk_data(out, data):
-	out.write(data)
+    out.write(data)
 
 
 if __name__ == "__main__":
